f72ba3410b4843099d47dfb9a9058ba1f3a728ea
inwoohub <dlsdndls@naver.com>
2026-06-11 17:43:52 +0900

    [Setting] : 스웨거 config 추가 및 application-local.yml 추가

---
+++ src/main/java/com/bbd/user/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.bbd.user.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("User API")
+                        .description("User Application API Documentation")
+                        .version("v1.0"))
+                .addServersItem(new Server()
+                        .url("http://localhost:8081/user")
+                        .description("Local 내 컴퓨터"))
+
+                .addServersItem(new Server()
+                        .url("http://100.73.142.41:8081/user")
+                        .description("강의실 노트북"))
+
+                .addServersItem(new Server()
+                        .url("http://192.168.200.220/user")
+                        .description("Nginx"));
+
+    }
+}
