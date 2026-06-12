f3bf16570d737772d9722432ac22930960291768
Choi-Siwon <c000317@naver.com>
2026-06-12 09:43:38 +0900

    #31 [Feat] : REDIS에 접근 삭제

--- src/main/java/com/bbd/user/application/port/out/CacheUserSnapshotPort.java
+++
@@ -1,8 +0,0 @@
-package com.bbd.user.application.port.out;
-
-import com.bbd.user.application.model.UserSnapshotResult;
-
-public interface CacheUserSnapshotPort {
-
-    void save(UserSnapshotResult snapshot);
-}
